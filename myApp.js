18608307d7106207452da564e701e75daf89a8b2
LionelBrunoDevSecOps <sighe82@yahoo.fr>
2024-02-06 04:38:12 +0200

    Add helmet dns prefetchcontrol

--- myApp.js
+++ myApp.js
@@ -10,6 +10,8 @@ app.use(helmet.ieNoOpen());
 
 const ninetyDaysInSeconds = 90*24*60*60 ;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true }))
+app.use(helmet.dnsPrefetchControl())
+
 
 
 
